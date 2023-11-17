330ad6a24330ed8e3520255820eafb6ddf008868
Volodymyr <volodymyrfilon1312@gmail.com>
2023-11-17 17:43:03 +0100

    upper header

--- src/components/icons/icons.const.ts
+++ src/components/icons/icons.const.ts
@@ -1,5 +1,11 @@
+import HeaderLocation from '../../../public/icons/header/header-location.svg';
+import HeaderMail from '../../../public/icons/header/header-mail.svg';
+import HeaderPhone from '../../../public/icons/header/header-phone.svg';
 import VerselIcon from '../../../public/vercel.svg';
 
 export const ICONS = {
   VERSEL: VerselIcon,
+  HEADER_LOCATION: HeaderLocation,
+  HEADER_MAIL: HeaderMail,
+  HEADER_PHONE: HeaderPhone,
 };
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,12 +1,30 @@
+import { ICONS } from '@/components/icons';
+import Link from 'next/link';
 import { HeaderProps } from './Header.props';
 
 export const Header: React.FC<HeaderProps> = ({ data }) => {
   const { nav } = data;
   return (
-    <header>
+    <header className="font-eUkraine">
       <div className="container">
-        Header
-        <div>{nav[0].name}</div>
+        <div className="flex h-auto items-center justify-between border-b py-4">
+          {/* Upper Nav */}
+          <Link className="flex text-base font-normal" href="/">
+            <ICONS.HEADER_LOCATION className="mr-2 h-6 w-6" />
+            816 North Normandie ave., Los Angeles
+          </Link>
+          <Link className="flex" href="tel:+16572140272">
+            <ICONS.HEADER_PHONE className="mr-2 h-6 w-6" />
+            +1 657-214-0272
+          </Link>
+          <Link className="flex" href="mailto:post.angeles.info@gmail.com">
+            <ICONS.HEADER_MAIL className="mr-2 h-6 w-6" />
+            post.angeles.info@gmail.com
+          </Link>
+        </div>
+
+        {/* Top Nav */}
+        <div className="flex h-6 items-center border-b">dwedefe</div>
       </div>
     </header>
   );
