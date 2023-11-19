2e0292043646196ee325cd4f5e4b10a946a8dbce
EITalanova <elenabelichenko77@gmail.com>
2023-11-19 12:24:09 +0200

    Add Logo component

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { FooterProps } from './Footer.props';
+import { Logo } from '../Logo/Logo';
 
 export const Footer: React.FC<FooterProps> = ({ data }) => {
   const { footerNav } = data;
@@ -6,6 +7,7 @@ export const Footer: React.FC<FooterProps> = ({ data }) => {
   return (
     <footer>
       <div className="container">
+        <Logo type='dark'/>
         Footer
         <div>{footerNav[0].name}</div>
       </div>
--- src/layout/Logo/Logo.tsx
+++ src/layout/Logo/Logo.tsx
@@ -1,3 +1,15 @@
-export const Logo = () => {
-  return <div>Logo</div>;
+import Link from '../../../node_modules/next/link';
+
+import { ICONS } from '../../components/icons/icons.const';
+import { LogoProps } from './Logo.props';
+
+export const Logo: React.FC<LogoProps> = ({ type }) => {
+  return (
+    <Link href="/">
+   {type === 'dark' ? (
+        <ICONS.LOGODARK className="h-10" />
+      ) : (
+        <ICONS.LOGOLIGHT className="h-10" />
+      )}    </Link>
+  );
 };
