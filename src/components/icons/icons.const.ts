466a3b72331f496669ff80bc865c0bd9d7ee91ec
Roman Martyniak <110495171+Roman-Martyniak@users.noreply.github.com>
2023-11-19 20:57:49 +0200

    contacts-section

--- src/components/icons/icons.const.ts
+++ src/components/icons/icons.const.ts
@@ -1,5 +1,15 @@
 import VerselIcon from '../../../public/vercel.svg';
+import FacebookIcon from '../../../public/icons/contacts/Facebook.svg';
+import InstagramIcon from '../../../public/icons/contacts/Instagram.svg';
+import MailIcon from '../../../public/icons/contacts/Mail.svg';
+import PhoneIcon from '../../../public/icons/contacts/Phone.svg';
+import LocationIcon from '../../../public/icons/contacts/Location.svg';
 
 export const ICONS = {
   VERSEL: VerselIcon,
+  FACEBOOK: FacebookIcon,
+  INSTAGRAM: InstagramIcon,
+  MAIL: MailIcon,
+  PHONE: PhoneIcon,
+  LOCATION: LocationIcon,
 };
--- src/layout/Contacts/Contacts.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const Contacts = (props: Props) => {
-  return <div>Contacts</div>;
-};
-
-export default Contacts;
