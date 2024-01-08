1b323ce9718bf3ca9f440c362663fb313c16edc4
Annka-Sarviro <annka_nuha@i.ua>
2024-01-08 11:27:29 +0200

    fix: fix footer container width

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC<FooterProps> = ({ data, lang, modal }) => {
 
   return (
     <footer className="bg-grey-120 pb-[34px] pt-5 md:pb-[62px] md:pt-[63px] lg:pb-[72px] lg:pt-[72px] xl:pb-[49px] xl:pt-12">
-      <div className="container ml-0 flex w-80 flex-col gap-5 font-eUkraine md:ml-auto md:w-fit md:gap-[29px] lg:gap-[30px] xl:gap-[101px]">
+      <div className="container flex flex-col gap-5 font-eUkraine md:ml-auto md:gap-[29px] lg:gap-[30px] xl:gap-[101px]">
         <div className="flex items-center justify-between text-base font-normal uppercase text-grey-0">
           <Logo type="light" lang={lang} />
 
@@ -23,7 +23,7 @@ export const Footer: React.FC<FooterProps> = ({ data, lang, modal }) => {
         </div>
 
         <div className="flex flex-col justify-start gap-5 font-normal text-grey-70 md:flex-row md:gap-10 xl:gap-[159px]">
-          <div className="md:w-[171px] lg:w-[293px] xl:w-fit">
+          <div className=" sm:w-[296px] md:w-[171px] lg:w-[293px] xl:w-fit">
             <Paragraph variantFontSize="14" variant="light_grey">
               {footerCop.name}
             </Paragraph>
