84e0c0e22cd42556afbb8a906fed0fbb34ccb22e
EITalanova <elenabelichenko77@gmail.com>
2024-01-15 12:02:52 +0200

    fix: img sizes

--- src/components/Participant/ParticipantHero/ParticipantHero.tsx
+++ src/components/Participant/ParticipantHero/ParticipantHero.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import Image from 'next/image';;
+import Image from 'next/image';
 
 import { Title } from '@/components/typography/Title/Title';
 import { Paragraph } from '@/components/typography/Paragraph/Paragraph';
-import useBreakpoints from '@/utils/useBreakpoints';
 import { ParticipantHeroProps } from './ParticipantHero.props';
 
 export const ParticipantHero: React.FC<ParticipantHeroProps> = ({
@@ -12,13 +11,8 @@ export const ParticipantHero: React.FC<ParticipantHeroProps> = ({
   accentTitle,
   description,
   img,
-  alt
+  alt,
 }) => {
-  const { less480px, less768px, less1000px, less1280px } = useBreakpoints();
-
-  const imageWidth = less480px ? 320 : less768px ? 438 : less1000px ? 728 : less1280px ? 960 : 780;
-  const imageHeight = less480px ? 205 : less768px ? 281 : less1000px ? 468 : less1280px ? 620 : 414;
-  
   return (
     <div className="container flex flex-col gap-[40px] pt-[113px] xl:flex-row xl:justify-between xl:pt-[188px]">
       <div className="flex flex-col gap-[30px] 2xl:gap-4">
@@ -30,13 +24,13 @@ export const ParticipantHero: React.FC<ParticipantHeroProps> = ({
           {description}
         </Paragraph>
       </div>
-    
+
       <Image
         className="rounded-2xl"
         src={`/images/participant/${img}.png`}
         alt={alt}
-        width={imageWidth}
-        height={imageHeight}
+        width={960}
+        height={620}
       />
     </div>
   );
