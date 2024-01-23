e6708a7e1ab47e1ae680c0ca03b4422a4187d730
Roman Martyniak <110495171+Roman-Martyniak@users.noreply.github.com>
2024-01-23 13:06:35 +0200

    fix: photo

--- src/sections/Contacts/Contacts.tsx
+++ src/sections/Contacts/Contacts.tsx
@@ -21,7 +21,7 @@ export const Contacts: React.FC<ContactsProps> = ({ data }) => {
           <ContactList />
         </div>
 
-        <div className="notMd:hidden  md:h-[416px] md:w-[349px] lg:w-[465px] xl:w-[687px]">
+        <div className="md:h-[416px] md:w-[349px] lg:w-[465px] xl:w-[687px] 2xl:h-[468px] 2xl:w-[664px] 3xl:h-[698px] 3xl:w-[1216px] notMd:hidden">
           <Image
             className="!relative rounded-2xl object-cover"
             src={Partners}
