7ffe42cd8845373e4e03e47ca72bad929d095d9f
Volodymyr <volodymyrfilon1312@gmail.com>
2024-01-25 18:01:12 +0100

    fix: contact page

--- src/app/[lang]/(withLayout)/contacts/page.tsx
+++ src/app/[lang]/(withLayout)/contacts/page.tsx
@@ -1,4 +1,5 @@
 import { ContactList } from '@/components/ContactList';
+import { ICONS } from '@/components/icons';
 import { Title } from '@/components/typography/Title';
 import { Locale } from '@/i18n.config';
 import { getDictionary } from '@/lib/dictionary';
@@ -10,25 +11,28 @@ const page = async ({ params: { lang } }: { params: { lang: Locale } }) => {
   const { contactsSection } = common.contacts;
 
   return (
-    <section className="mb-72 mt-[258px]">
-      <div className="container flex justify-between border-b pb-[140px]">
-        <div>
-          <Title tag="h2" variantSize="h2" className="pb-[50px] ">
-            {contactsSection.title}
-            <span>
-              <br /> {contactsSection.title1}
-            </span>
+    <section className="mb-[100px] mt-[102px] xl:mt-[62px]">
+      <div className="container">
+        <div className="flex flex-col gap-y-[30px] border-b border-grey-60 pb-10">
+          <Title tag="h2" variantSize="h2" className="flex">
+            <p>{contactsSection.title} &zwnj;</p>
+            <span>{contactsSection.accentTitle}</span>
           </Title>
           <ContactList />
-        </div>
 
-        <Image
-          className="rounded-2xl"
-          src={Partners}
-          width={790}
-          height={540}
-          alt={contactsSection.img.alt}
-        />
+          <div className="relative">
+            <Image
+              className="rounded-2xl "
+              src={Partners}
+              width={790}
+              height={540}
+              alt={contactsSection.img.alt}
+            />
+            <div className="absolute right-[31%] top-[32%] h-10 w-10 sm:h-[60px] sm:w-[60px] ">
+              <ICONS.LOGO_SMALL />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
