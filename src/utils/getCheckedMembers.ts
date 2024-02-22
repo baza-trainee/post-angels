4668ba07c084432e1eaff45be3ec6cc1435e841c
Annka-Sarviro <annka_nuha@i.ua>
2024-02-22 15:19:08 +0200

    fix:slider btn

--- src/utils/getCheckedMembers.ts
+++ src/utils/getCheckedMembers.ts
@@ -4,8 +4,17 @@ import useBreakpoints from '@/utils/useBreakpoints';
 import { TeamMember } from '@/components/OurTeam/TeamMemberCard/TeamMemberCard.props';
 
 export const getCheckedMembers = (members: TeamMember[]) => {
-  const defaultMembers = {
-    id: 'default',
+  const defaultMembers1 = {
+    id: 'default1',
+    name: '',
+    lastName: '',
+    image: {
+      alt: '',
+      url: '',
+    },
+  };
+  const defaultMembers2 = {
+    id: 'default2',
     name: '',
     lastName: '',
     image: {
@@ -17,14 +26,14 @@ export const getCheckedMembers = (members: TeamMember[]) => {
   const { bigger768px, less1280px, bigger1280px } = useBreakpoints();
   if (bigger768px && less1280px) {
     bigger768px && less1280px && members.length % 2 !== 0
-      ? checkedTeamMembers.push(...members, defaultMembers)
+      ? checkedTeamMembers.push(...members, defaultMembers1)
       : checkedTeamMembers.push(...members);
   } else if (bigger1280px) {
     members.length % 3 === 0
       ? checkedTeamMembers.push(...members)
       : members.length % 3 === 1
-        ? checkedTeamMembers.push(...members, defaultMembers, defaultMembers)
-        : checkedTeamMembers.push(...members, defaultMembers);
+        ? checkedTeamMembers.push(...members, defaultMembers1, defaultMembers2)
+        : checkedTeamMembers.push(...members, defaultMembers1);
   }
 
   return checkedTeamMembers;
