e3eb763102837a778f0179f58eba5de1378f58fe
Annka-Sarviro <annka_nuha@i.ua>
2024-03-08 12:10:15 +0200

    fix build doc error

--- src/sections/ProjectPage/ProjectDocument/ProjectDocument.tsx
+++ src/sections/ProjectPage/ProjectDocument/ProjectDocument.tsx
@@ -1,32 +1,39 @@
 import { ProjectLinks } from '@/components/project/ProjectLinks';
-import { ProjectDataInfoProps, ProjectDocumentProps, StaticDataProps } from '../ProjectPage.props';
+import {
+  ProjectDataInfoProps,
+  ProjectDocumentProps,
+  ProjectOneDataProps,
+  ProjectOneDataType,
+  StaticDataProps,
+} from '../ProjectPage.props';
 import { Paragraph } from '@/components/typography/Paragraph';
 import { Supported } from '@/components/project/Supported';
 import { FileIcon, defaultStyles } from 'react-file-icon';
 import Link from 'next/link';
 
 export const ProjectDocument = ({
-  projectDataDoc,
+  projectInfoData,
   project_details,
-  projectSupportsData,
 }: {
-  projectDataDoc: ProjectDocumentProps[];
-  projectSupportsData: ProjectDataInfoProps[];
+  projectInfoData: ProjectOneDataProps;
+
   project_details: StaticDataProps;
 }) => {
+  const projectDataDoc = projectInfoData?.projects?.attributes?.document;
+  const projectSupportsData = projectInfoData.supportsData;
   return (
     <section className="pb-[100px]">
       <div className="container">
         <ProjectLinks project_details={project_details.link_nav} variant="documents" />
         <div className="flex flex-col justify-between gap-y-[50px]  pt-[40px] xl:flex-row xl:gap-x-8 xl:pt-[30px]">
           <div className="grid h-fit w-full grid-cols-1 gap-5 md:grid-cols-2">
-            {projectDataDoc.length > 0 ? (
+            {projectDataDoc && projectDataDoc.length !== 0 ? (
               projectDataDoc.map(doc => {
-                const ext = doc.href.data[0].attributes.ext.replace(
+                const ext = doc.href.data[0]?.attributes.ext.replace(
                   /^\./,
                   ''
                 ) as keyof typeof defaultStyles;
-                const fileIconProps = defaultStyles[ext];
+                const fileIconProps = ext ? defaultStyles[ext] : defaultStyles['pdf'];
 
                 return (
                   <Link
