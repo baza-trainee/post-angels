d00610c43253be75ab971308bdae9a1bfe5a2d29
Volodymyr <volodymyrfilon1312@gmail.com>
2024-04-09 15:00:48 +0200

    fix: about in numbers

--- src/api/fetchAboutInNumbers.ts
+++ src/api/fetchAboutInNumbers.ts
@@ -4,9 +4,19 @@ import { Locale } from '@/i18n.config';
 import { AboutNumberProps } from '@/sections/AboutInNumbers/AboutInNumbers.props';
 import { getAboutInNumbers } from './requests/getAboutInNumbers';
 
+interface AboutNumberDataType {
+  aboutUsSection: {
+    data: {
+      attributes: {
+        about: AboutNumberProps[];
+      };
+    };
+  };
+}
+
 export const fetchAboutInNumbers = async (locale: Locale): Promise<AboutNumberProps[]> => {
   try {
-    const data: any = await request(
+    const data: AboutNumberDataType = await request(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/graphql` as string,
       getAboutInNumbers,
       {
@@ -18,13 +28,12 @@ export const fetchAboutInNumbers = async (locale: Locale): Promise<AboutNumberPr
     );
 
     const result = data.aboutUsSection.data.attributes.about;
-
     return result;
   } catch (error) {
     return {
       name: '',
       measure: '',
-      quantiy: '',
+      quantity: '',
       title: '',
     } as any;
   }
