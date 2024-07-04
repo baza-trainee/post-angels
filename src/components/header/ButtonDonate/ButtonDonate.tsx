7effe7124a7aad3609a4088762d72cc50f46abf0
Annka-Sarviro <annka_nuha@i.ua>
2024-07-04 11:08:20 +0300

    fix scroll header

--- src/components/header/ButtonDonate/ButtonDonate.tsx
+++ src/components/header/ButtonDonate/ButtonDonate.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/buttons/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ModalChildDonate from '@/components/ModalChildDonate/ModalChildDonate';
 import Modal from '@/components/modal/Modal';
@@ -21,6 +21,42 @@ export const ButtonDonate = ({
   const handleClick = () => {
     setIsModalOpen(true);
   };
+
+  useEffect(() => {
+    let lastScrollTop = 0;
+    const targetElement = document.getElementById('header');
+    const topHeader = document.getElementById('top_header');
+
+    if (!targetElement || !topHeader) {
+      console.error('Target elements not found');
+      return;
+    }
+
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        targetElement.classList.remove('xl:translate-y-0');
+        targetElement.classList.add('xl:translate-y-[-60px]');
+        topHeader.classList.add('xl:py-4');
+      } else {
+        // Scrolling up
+        targetElement.classList.remove('xl:translate-y-[-60px]');
+        targetElement.classList.add('xl:translate-y-0');
+        topHeader.classList.remove('xl:py-4');
+      }
+
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
